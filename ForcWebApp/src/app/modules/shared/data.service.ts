3a55d8b6754d9916fbe8d92ef47ac4c818cc043f
GricivRoman <griciv.roman.st@gmail.com>
2023-09-21 10:05:49 +0300

    Базовый круд сервис

---
+++ ForcWebApp/src/app/modules/shared/data.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Guid } from "guid-typescript";
+import { Observable } from "rxjs";
+import { BaseEntity } from "./baseEntity";
+
+@Injectable()
+export class DataService<T extends BaseEntity> {
+    public url: string;
+    
+    constructor(private http: HttpClient){
+    }
+    
+    public get(id: Guid): Observable<T>{
+        return this.http.get<T>(`${this.url}/${id}`);
+    }
+
+    public getList(): Observable<T[]>{
+        return this.http.get<T[]>(`${this.url}/list`);
+    }
+
+    public save(model: T){
+        return this.http.post(`${this.url}`, model);
+    }
+
+    public update(model: T){
+        return this.http.put(`${this.url}`, model);
+    }
+
+    public delete(id: Guid): Observable<any>{
+        return this.http.delete(`${this.url}/${id}`);
+    }
+}
