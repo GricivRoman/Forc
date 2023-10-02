f6d25273a404497839b9abed8014db9e778d47bb
GricivRoman <griciv.roman.st@gmail.com>
2023-10-02 17:34:06 +0300

    Сервис файловое хранилище

---
+++ Froc.FileStorage/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace Forc.FileStorage
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("allowedOrigins", policy =>
+                {
+                    policy.WithOrigins(_configuration["WebAPIAplicationURL"]).AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseCors("allowedOrigins");
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Default",
+                    "/{controller}/{action}/{id?}");
+            });
+        }
+    }
+}
