ad1a8012e4c4a0671f8fdae9fe056cdc6e63accb
GricivRoman <griciv.roman.st@gmail.com>
2023-10-12 14:17:27 +0300

    Базовая инфраструктура для интеграционных тестов

---
+++ Forc.IntegtationTests/Infrastructure/IntegrationTest.cs
@@ -0,0 +1,41 @@
+﻿using Forc.Infrastructure.Data;
+using Forc.IntegtationTests.Infrastructure;
+using Forc.WebApi;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Forc.IntegtationTests
+{
+    public class IntegrationTest : IDisposable, IClassFixture<TestWebApplicationFactory<Program>>
+    {
+        private readonly TestWebApplicationFactory<Program> _factory;
+        private bool _disposedValue;
+
+        public IntegrationTest(TestWebApplicationFactory<Program> factory)
+        {
+            using (var scope = factory.Services.CreateScope())
+            {
+                scope.ServiceProvider.GetService<DataContext>().Database.EnsureCreated();
+            }
+            _factory = factory;
+        }
+        ~IntegrationTest() => Dispose(false);
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _factory.Services.CreateScope().ServiceProvider.GetService<DataContext>().Database.EnsureDeleted();
+                }
+                _disposedValue = true;
+            }
+        }
+    }
+}
---
+++ Forc.IntegtationTests/Infrastructure/TestWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+﻿using Forc.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Forc.IntegtationTests.Infrastructure
+{
+    public class TestWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        private readonly string ConnectionString = "SERVER=localhost; Port=5432; Database=Forc_test; Username=postgres; Password=postgres;";
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<DataContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<DataContext>(opt =>
+                {
+                    opt.UseNpgsql(ConnectionString);
+                });
+            });
+
+            return base.CreateHost(builder);
+        }
+    }
+}
